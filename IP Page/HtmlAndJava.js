10f2e47f1a3ab5c5326051fdd3c243cd78f22b2f
Bruno <dancevtm@gmail.com>
2021-10-27 16:26:11 +0200

    Last Homework Html JavaScript with Tables

---
+++ IP Page/HtmlAndJava.js
@@ -0,0 +1,156 @@
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var TimeNow = document.getElementById("Time")
+TimeNow.innerHTML = "Current time is " + time;
+
+
+var element1 = document.getElementById("Element1");
+var NumElement1 = Math.sqrt(1);
+element1.innerHTML = NumElement1 ;
+
+var OddOrEven1 = document.getElementById("OddOrEven1");
+
+if(NumElement1 % 2 == 0) {
+    OddOrEven1.innerHTML = "Even";
+}
+
+else {
+    OddOrEven1.innerHTML = "Odd";
+}
+
+
+var element2 = document.getElementById("Element2");
+var NumElement2 = Math.sqrt(2);
+element2.innerHTML = NumElement2 ;
+
+var OddOrEven2 = document.getElementById("OddOrEven2");
+if(NumElement2 % 2 == 0) {
+    OddOrEven2.innerHTML = "Even";
+}
+
+else {
+    OddOrEven2.innerHTML = "Odd";
+}
+
+
+var element3 = document.getElementById("Element3");
+var NumElement3 = Math.sqrt(3);
+element3.innerHTML = NumElement3 ;
+
+var OddOrEven3 = document.getElementById("OddOrEven3");
+if(NumElement3 % 2 == 0) {
+    OddOrEven3.innerHTML = "Even";
+}
+
+else {
+    OddOrEven3.innerHTML = "Odd";
+}
+
+
+var element4 = document.getElementById("Element4");
+var NumElement4 = Math.sqrt(4);
+element4.innerHTML = NumElement4 ;
+
+var OddOrEven4 = document.getElementById("OddOrEven4");
+if(NumElement4 % 2 == 0) {
+    OddOrEven4.innerHTML = "Even";
+}
+
+else {
+    OddOrEven4.innerHTML = "Odd";
+}
+
+
+var element5 = document.getElementById("Element5");
+var NumElement5 = Math.sqrt(5);
+element5.innerHTML = NumElement5 ;
+
+var OddOrEven5 = document.getElementById("OddOrEven5");
+if(NumElement5 % 2 == 0) {
+    OddOrEven5.innerHTML = "Even";
+}
+
+else {
+    OddOrEven5.innerHTML = "Odd";
+}
+
+
+var element6 = document.getElementById("Element6");
+var NumElement6 = Math.sqrt(6);
+element6.innerHTML = NumElement6 ;
+
+var OddOrEven6 = document.getElementById("OddOrEven6");
+if(NumElement6 % 2 == 0) {
+    OddOrEven6.innerHTML = "Even";
+}
+
+else {
+    OddOrEven6.innerHTML = "Odd";
+}
+
+
+
+
+var element7 = document.getElementById("Element7");
+var NumElement7 = Math.sqrt(7);
+element7.innerHTML = NumElement7 ;
+
+var OddOrEven7 = document.getElementById("OddOrEven7");
+if(NumElement7 % 2 == 0) {
+    OddOrEven7.innerHTML = "Even";
+}
+
+else {
+    OddOrEven7.innerHTML = "Odd";
+}
+
+
+var element8 = document.getElementById("Element8");
+var NumElement8 = Math.sqrt(8);
+element8.innerHTML = NumElement8 ;
+
+var OddOrEven8 = document.getElementById("OddOrEven8");
+if(NumElement6 % 2 == 0) {
+    OddOrEven8.innerHTML = "Even";
+}
+
+else {
+    OddOrEven8.innerHTML = "Odd";
+}
+
+
+
+
+var element9 = document.getElementById("Element9");
+var NumElement9 = Math.sqrt(9);
+element9.innerHTML = NumElement9 ;
+
+var OddOrEven9 = document.getElementById("OddOrEven9");
+if(NumElement9 % 2 == 0) {
+    OddOrEven9.innerHTML = "Even";
+}
+
+else {
+    OddOrEven9.innerHTML = "Odd";
+}
+
+
+var element10 = document.getElementById("Element10");
+var NumElement10 = Math.sqrt(10);
+element10.innerHTML = NumElement10 ;
+
+var OddOrEven10 = document.getElementById("OddOrEven10");
+if(NumElement10 % 2 == 0) {
+    OddOrEven10.innerHTML = "Even";
+}
+
+else {
+    OddOrEven10.innerHTML = "Odd";
+}
+
+
+
+
+
+
+
